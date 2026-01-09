d86a8e9736962e3a95e28f4c1442c1b9a76b38f7
ThammaratPul82 <thammarat827000@gmail.com>
2026-01-09 16:48:35 +0700

    completed

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -4,7 +4,7 @@ using namespace std;
 template <typename T>
 void insertionSort(T d[],int N){
 	for (int i = 1; i < N; ++i) {
-        int key = d[i];
+        T key = d[i];
         int j = i - 1;
 		while (j >= 0 && d[j] < key) {
             d[j + 1] = d[j];
